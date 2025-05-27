a61caf207f0c160f92112425c1f97176ba94517b
durga shankar pandey <51326341+Durga45@users.noreply.github.com>
2025-05-27 19:13:16 +0530

    Update user.auth.js

--- src/middleware/user.auth.js
+++ src/middleware/user.auth.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 export const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  // Check for "Bearer <token>" format
+ 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: Token missing or malformed" });
   }
 
-  const token = authHeader.split(" ")[1]; // Extract token after "Bearer"
+  const token = authHeader.split(" ")[1]; 
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_KEY);
